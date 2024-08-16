3542b5d51aff7a655f7f3f522dcff4f654980ff7
hidden <hidden>
2024-08-16 15:41:49 +0500

    Use reducer

--- src/page.js
+++ src/page.js
@@ -1,16 +1,38 @@
-// Page.js
-import React, { useContext } from 'react';
-import { CounterContext } from './component1';
- //value={light}
-function Page() {
-  const { count, increment, decrement } = useContext(CounterContext);
-  return (
-   <div>
-    <h2>Counter Value is :{count}  </h2>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
-   </div>
-  );
+import {  useReducer } from "react";
+
+function MyApp(){
+
+  function reducer(state,action){
+    console.log(action.type)
+    console.log("ACTION",action)
+      switch(action.type){
+        case 'increment':
+        return { count: state.count + 1 ,hidden:state.hidden=true};
+      case 'decrement':
+        return { count: state.count - 1 };
+      case 'reset':
+        return { count: 0  };
+      default:
+        throw new Error();
+    }
+  }
+
+  const initialState = { count: 0 ,hidden:false};
+  const [state,dispatch]=useReducer(reducer,initialState)
+
+  return(
+    <>
+    <div>Counter {state.count}</div>
+    <button onClick={() => dispatch({ type: 'increment' })}>Click to Increment</button>
+    <button onClick={() => dispatch({ type: 'decrement' })}>Click to Decrement</button>
+    {state.hidden ? 'Show Counter' : 'Hide Counter'}
+    </>
+  )
+
+
+
+  
 }
 
-export default Page;
+
+export default MyApp;
